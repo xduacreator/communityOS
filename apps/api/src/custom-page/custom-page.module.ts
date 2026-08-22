3fb101e6faf1233e8e0ba08af65c5f5a5fdfbc4b
setyadi aprilianto <setyadiaprilianto@192.168.1.22>
2026-08-22 07:13:01 +0700

    fix: import PrismaModule in CustomPageModule

--- apps/api/src/custom-page/custom-page.module.ts
+++ apps/api/src/custom-page/custom-page.module.ts
@@ -1,8 +1,10 @@
 import { Module } from '@nestjs/common';
 import { CustomPageService } from './custom-page.service';
 import { CustomPageController } from './custom-page.controller';
+import { PrismaModule } from '../prisma/prisma.module';
 
 @Module({
+  imports: [PrismaModule],
   controllers: [CustomPageController],
   providers: [CustomPageService],
   exports: [CustomPageService],
